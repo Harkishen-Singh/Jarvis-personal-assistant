718462ec046a285832356168062bedfcf0896881
muskankhedia <muskan.khedia2000@gmail.com>
2019-03-22 21:32:22 +0530

    fixed eslint errors

--- view/app-jarvis.js
+++ view/app-jarvis.js
@@ -74,35 +74,33 @@ app.controller('MainController', function($scope,$location,$rootScope,$http) {
 });
 
 
-    var recognizing;
-    var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    reset();
-    recognition.onend = reset;
+var recognizing;
+var recognition = new webkitSpeechRecognition();
+recognition.continuous = true;
+reset();
+recognition.onend = reset;
 
-    recognition.onresult = function (event) {
-		console.log("sdaf")
-        for (var i = event.resultIndex; i < event.results.length; ++i) {
-            if (event.results[i].isFinal) {
-                document.getElementById("message-input").innerHTML += event.results[i][0].transcript;
-            }
-        }
-		console.log("done")
-    }
+recognition.onresult = function (event) {
+	for (var i = event.resultIndex; i < event.results.length; ++i) {
+		if (event.results[i].isFinal) {
+			document.getElementById('message-input').innerHTML += event.results[i][0].transcript;
+		}
+	}
+};
 
-    function reset() {
-        recognizing = false;
-        button.innerHTML = "Click to Speak";
-    }
+function reset() {
+	recognizing = false;
+	button.innerHTML = 'Click to Speak';
+}
 
-    function toggleStartStop() {
-        if (recognizing) {
-            recognition.stop();
-            reset();
-        } else {
-            recognition.start();
-            recognizing = true;
-            button.innerHTML = "Click to Stop";
-        }
-    }
+function toggleStartStop() {
+	if (recognizing) {
+		recognition.stop();
+		reset();
+	} else {
+		recognition.start();
+		recognizing = true;
+		button.innerHTML = 'Click to Stop';
+	}
+}
 
