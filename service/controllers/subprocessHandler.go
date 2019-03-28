5184542a5157e35e5ce6ac97094e23722801a6c7
Harkishen-Singh <harkishensingh@hotmail.com>
2019-03-29 01:04:34 +0530

    subprocesses support for scrapping of data

--- service/controllers/subprocessHandler.go
+++ service/controllers/subprocessHandler.go
@@ -18,7 +18,7 @@ func HandlerGoogle(method string, url string) string {
 
 	directory, _ := os.Getwd()
 	fmt.Println("method -> " + method + " url -> " + url + " direc -> " + directory)
-	result, err := exec.Command(directory + "/subprocesses", "node", "google.js", method, url).Output()
+	result, err := exec.Command("node", "subprocesses/google.js", method, url).Output()
 	// result, err := exec.Command("pwd").Output()
 	if err != nil {
 		panic(err)
