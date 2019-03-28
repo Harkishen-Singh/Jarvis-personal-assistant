e6a808e9e7752a41100d92b399005726bb2ea29c
Harkishen-Singh <harkishensingh@hotmail.com>
2019-03-29 01:00:52 +0530

    added subprocesses

--- service/controllers/messages.go
+++ service/controllers/messages.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"net/http"
+	"strings"
+	"fmt"
 )
 
 type response struct {
@@ -19,6 +21,69 @@ func MessagesController(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	r.ParseForm()
-	w.Write([]byte(`{"status": "success", "message": "Hi from reply bot"}`))
 
+	request := response{
+		username: r.FormValue("username"),
+		message: r.FormValue("message"),
+	}
+
+	routes(request, w)
+
+	// w.Write([]byte(`{"status": "success", "message": "Hi from reply bot"}`))
+
+}
+
+func routes(routeObject response, w http.ResponseWriter) {
+	fmt.Println("inside routes")
+
+	message := routeObject.message
+	// messageTemp := message
+	firstPars := message[:strings.Index(message, " ")]
+
+	strArr := strings.Split(firstPars, " ")
+	strArrDiff := strings.Split(message, " ")
+
+	messageExceptFirstPars := strings.Join(stringDifference(strArr, strArrDiff), " ")
+	// lastParsArr := strings.Split(messageTemp, " ")
+	// lastPars := lastParsArr[len(lastParsArr) - 1]
+
+	// single word operations
+
+	if firstPars == "google" { // for google search
+
+		query := "https://www.google.co.in/search?q=" + messageExceptFirstPars
+		result := HandlerGoogle("GET", query)
+		fmt.Println(result)
+
+	}
+
+}
+
+// gives the difference of two string arrays as an array of the differed element
+func stringDifference(slice1 []string, slice2 []string) []string {
+    var diff []string
+
+    // Loop two times, first to find slice1 strings not in slice2,
+    // second loop to find slice2 strings not in slice1
+    for i := 0; i < 2; i++ {
+        for _, s1 := range slice1 {
+            found := false
+            for _, s2 := range slice2 {
+                if s1 == s2 {
+                    found = true
+                    break
+                }
+            }
+            // String not found. We add it to return slice
+            if !found {
+                diff = append(diff, s1)
+            }
+        }
+        // Swap the slices, only if it was the first loop
+        if i == 0 {
+            slice1, slice2 = slice2, slice1
+        }
+    }
+
+    return diff
 }
