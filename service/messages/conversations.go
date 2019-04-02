3aa59a05ad0b9c793c4fd964181b472eb3c133e4
Harkishen-Singh <harkishensingh@hotmail.com>
2019-04-03 02:12:34 +0530

    basic general conv

--- service/messages/conversations.go
+++ service/messages/conversations.go
@@ -1,15 +1,23 @@
 package messages
 
 import (
-	// "fmt"
+	"fmt"
 	"net/http"
-	// "encoding/json"
-	// "os"
+	"encoding/json"
+	"os"
+	"io/ioutil"
+	"math/rand"
 )
 
 type response struct {
 	username string
 	message string
+}
+
+type jsonResponse struct {
+	Status bool `json:"status"`
+	Message string `json:"message"`
+	Result []string `json:"result"`
 }
 
 // Messages json parser for default string types
@@ -27,15 +35,80 @@ type Messagesreplies struct {
 var (
 	messagesParser Messages
 	messagesRepliesParser Messagesreplies
+	resp jsonResponse
 )
 
 func loadJSONParsers() {
+
+	fmt.Println("Loading JSON parsers....")
+	messagesFile, err := os.Open("messages.json")
+	messagesRepliesFile, err2 := os.Open("messages_replies.json")
+	bytvalMF, _ := ioutil.ReadAll(messagesFile)
+	bytvalMRF, _ := ioutil.ReadAll(messagesRepliesFile)
+	if err != nil   {
+		panic(err)
+	}
+	if err2 != nil   {
+		panic(err2)
+	}
+
+	json.Unmarshal(bytvalMF, &messagesParser)
+	json.Unmarshal(bytvalMRF, &messagesRepliesParser)
 
 }
 
 // GeneralConvHandler handles stuff related to general conversation
 func GeneralConvHandler(req response, res http.ResponseWriter) {
 
+	loadJSONParsers()
+	message := req.message
+	// determine type of message
+	isGreetingPlain := func(s string) bool {
+		for i:=0; i< len(messagesParser.InitialGreetingsPlain); i++ {
+			if messagesParser.InitialGreetingsPlain[i] == s {
+				return true
+			}
+		}
+		return false
+	}(message)
+
+	if isGreetingPlain {
+		temp := greetingPlainController(message)
+		resp.Status = true
+		resp.Message = temp
+		marshalled, _ := json.Marshal(resp)
+		res.Write(marshalled)
+		return ;
+	}
+
+	isGreetingName := func(s string) bool {
+		for i:=0; i< len(messagesParser.InitialGreetingsName); i++ {
+			if messagesParser.InitialGreetingsName[i] == s {
+				return true
+			}
+		}
+		return false
+	}(message)
+
+	if isGreetingName {
+		temp := greetingNameController(message)
+		resp.Status = true
+		resp.Message = temp
+		marshalled, _ := json.Marshal(resp)
+		res.Write(marshalled)
+	}
+}
+
+func greetingPlainController(s string) string {
+
+	numb := rand.Intn(len(messagesRepliesParser.InitialGreetingsPlain))
+	return messagesRepliesParser.InitialGreetingsPlain[numb]
 
 }
 
+func greetingNameController(s string) string {
+
+	numb := rand.Intn(len(messagesRepliesParser.InitialGreetingsName))
+	return messagesRepliesParser.InitialGreetingsName[numb]
+
+}
