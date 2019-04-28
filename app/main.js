38f258cdb384a01e69b1c5d82616079935ae5733
Harkishen-Singh <harkishensingh@hotmail.com>
2019-04-29 01:45:27 +0530

    basic configuration

--- app/main.js
+++ app/main.js
@@ -1,4 +1,19 @@
-const electron = require('electron');
+const electron = require('electron'),
+    BrowserWindow = electron.BrowserWindow,
+    App = electron.app,
+    Menu = electron.Menu;
 
-let app;
+let mainWindow;
 
+function MainWindow() {
+    mainWindow = new BrowserWindow({
+        width: 400,
+        height: 600,
+        backgroundColor: '#fff',
+        resizable: false,
+        autoHideMenuBar: true
+    });
+    mainWindow.loadURL('file://'+__dirname+'/templates/index.html')
+}
+
+App.on('ready', MainWindow);
