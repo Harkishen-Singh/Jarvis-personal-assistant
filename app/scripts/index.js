bda8d22e91c42a70de805225b449c5d671718324
Harkishen-Singh <harkishensingh@hotmail.com>
2019-04-29 22:10:20 +0530

    added button for repeated questions

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -26,21 +26,26 @@ app.controller('area-controller', function ($scope) {
 	$scope.Initialize = function () {
 		$scope.showJarvisBotArea = true;
 		$scope.showLabel = true;
+		$scope.showReset = false;
 	};
 	$scope.AskButtonClick = function () {
 		$scope.showJarvisBotArea = false;
-		$scope.showLabel = false;
+		$scope.showLabel = !$scope.showLabel;
+		$scope.showReset = !$scope.showReset;
+		console.warn($scope);
 		let ele0 = document.getElementById('jarvis-bot-area-id'),
 			ele1 = document.getElementById('ele1-move'),
 			ele2 = document.getElementById('ele2-move'),
 			ele3 = document.getElementById('userButton'),
 			ele4 = document.getElementById('ele4-move'),
-			ele5 = document.getElementById('message-icon-style');
+			ele5 = document.getElementById('message-icon-style'),
+			ele6 = document.getElementById('jarvis-message-id');
 		ele0.classList.toggle('hide');
 		ele1.classList.toggle('logo-post-query');
 		ele2.classList.toggle('user-area-post-query');
 		ele3.classList.toggle('user-input-ask-button-post-query');
 		ele4.classList.toggle('user-input-outer-layer-post-query');
 		ele5.style.display = 'none';
+		ele6.classList.toggle('message-jarvis-bot-post-query');
 	};
 });
