39f0db1fdddb1162957c42f02546089c72e83039
Harkishen Singh <harkishensingh@hotmail.com>
2020-03-19 20:40:22 +0530

    use heroku port
    Signed-off-by: Harkishen Singh <harkishensingh@hotmail.com>

--- service/maintut.go
+++ service/maintut.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"os"
+
 	"github.com/Harkishen-Singh/Jarvis-personal-assistant/service/utils"
 )
 
 func main() {
-	utils.Server("3000")
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "3000"
+	}
+
+	utils.Server(port)
 }
