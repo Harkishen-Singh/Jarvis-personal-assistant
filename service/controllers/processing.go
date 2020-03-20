6da3ddbf4a10e2c122a502dd266db85007bfa964
muskankhedia <muskan.khedia2000@gmail.com>
2020-03-20 15:54:03 +0530

    added go fmt

--- service/controllers/processing.go
+++ service/controllers/processing.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 	"time"
 
-	scrapper "github.com/Harkishen-Singh/Jarvis-personal-assistant/service/utils" 
+	scrapper "github.com/Harkishen-Singh/Jarvis-personal-assistant/service/utils"
 )
 
 // processGoogleResponses scrapes the relevant Google search engine for SearchResults
--- service/controllers/text_to_speech.go
+++ service/controllers/text_to_speech.go
@@ -2,8 +2,10 @@ package controllers
 
 import (
 	"fmt"
-	"github.com/hegedustibor/htgo-tts"
 	"time"
+
+	"github.com/Harkishen-Singh/Jarvis-personal-assistant/service/logger"
+	"github.com/hegedustibor/htgo-tts"
 )
 
 // TextToSpeech converts text to speech on a string, return status as boolean
@@ -12,7 +14,10 @@ func TextToSpeech(message string, waitInSeconds int32) bool {
 	time.Sleep(time.Duration(waitInSeconds))
 	fmt.Println("Speaking -> ", message)
 	speech := htgotts.Speech{Folder: "audio", Language: "en"}
-	speech.Speak(message)
+	err := speech.Speak(message)
+	if (err != nil) {
+		logger.Error(err)
+	}
 	return true
 
 }
