476f47ddccb90a804f84e51bd7696fb2d7af4171
muskankhedia <muskan.khedia2000@gmail.com>
2020-04-05 20:03:20 +0530

    fix lint

--- src/service/handlers/ast.js
+++ src/service/handlers/ast.js
@@ -1,18 +1,16 @@
+/* eslint-disable new-cap */
 const queryController = require('./queryController').QueryController;
 
 class AST {
-
-    serialize(firstParam, secondParam) {
-        console.log("first param: ", firstParam)
-        const queryCont = new queryController();
-        if (firstParam == "google") {
-            var response = queryCont.HandleGoogleQuery(secondParam)
-                    console.log("response:: ", response)
-                    return response
-        }
-
+  serialize(firstParam, secondParam) {
+    console.log('first param: ', firstParam);
+    const queryCont = new queryController();
+    if (firstParam == 'google') {
+      const response = queryCont.HandleGoogleQuery(secondParam);
+      console.log('response:: ', response);
+      return response;
     }
-
+  }
 }
 
-module.exports = { AST }
+module.exports = { AST };
