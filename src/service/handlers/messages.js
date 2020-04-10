cad1766d41a32c5bb76143d3aaf5a91869000e49
Irshad Ansari <40335357+irshadjsr21@users.noreply.github.com>
2020-04-10 17:00:13 +0000

    Added meaning scrapping (#212)
    * Added meaning scrapping
    
    * Changed response keys to lowercase

--- src/service/handlers/messages.js
+++ src/service/handlers/messages.js
@@ -3,20 +3,21 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const results = [];
 class Message {
   async recMessage(req, res, next) {
     // const username = req.query.username;
     res.setHeader('Access-Control-Allow-Origin', '*');
     const message = req.query.message;
+    const results = [];
 
     if (message.length >= 2) {
       const messageArr = message.split(' ');
       const searchKind = messageArr[0];
       const queryParam = messageArr[1];
+      let SCRAPING_URL;
       switch (searchKind) {
         case 'google':
-          const SCRAPING_URL = 'https://www.google.com/search?q=' + queryParam;
+          SCRAPING_URL = 'https://www.google.com/search?q=' + queryParam;
           axios
               .get(SCRAPING_URL)
               .then((resd) => {
@@ -25,15 +26,59 @@ class Message {
                   const $ = cheerio.load(resd);
                   $('.BNeawe.vvjwJb.AP7Wnd').each(function() {
                     const details = new Object();
-                    details.Head = $(this).text();
+                    details.head = $(this).text();
                     console.log('details:: ', details);
                     results.push(details);
                   });
                 }
                 const response = {
-                  Status: true,
-                  Message: 'here are the top search results',
-                  Result: results
+                  status: true,
+                  message: 'here are the top search results',
+                  result: results
+                };
+                res.send(response);
+              })
+              .catch((err) => console.log(err));
+          break;
+        case 'meaning':
+          SCRAPING_URL =
+            'https://www.oxfordlearnersdictionaries.com/definition/english/' +
+            queryParam;
+          axios
+              .get(SCRAPING_URL)
+              .then((resd) => {
+                resd = resd.data;
+                if (resd) {
+                  const $ = cheerio.load(resd);
+                  $('ol > li').each(function() {
+                    const currentNode = $(this);
+                    const defNode = currentNode.find('span.def');
+                    let def;
+                    let example;
+
+                    if (defNode) {
+                      def = defNode.text();
+                    }
+
+                    const exampleNode = currentNode
+                        .find('ul.examples > li')
+                        .first();
+                    if (exampleNode) {
+                      example = exampleNode.text();
+                    }
+
+                    if (def) {
+                      const details = new Object();
+                      details.meaning = def;
+                      details.example = example;
+                      results.push(details);
+                    }
+                  });
+                }
+                const response = {
+                  status: true,
+                  message: 'here are the top search results',
+                  result: results
                 };
                 res.send(response);
               })
